b7039866fb86a54258096b3e50decc8981d76703
Andrey Zax <andrey.zax@gmail.com>
2022-10-17 16:20:19 +0300

    Set window resolution and game title

--- pong.py
+++ pong.py
@@ -2,11 +2,14 @@
 
 import pygame
 
+SCREEN_W = 1920
+SCREEN_H = 1200
 
 pygame.init()
 
-pygame.display.set_mode((1200,800))
-pygame.display.set_caption('PyGame')
+pygame.display.set_mode((SCREEN_W, SCREEN_H))
+pygame.display.set_caption('pong')
+
 
 clock = pygame.time.Clock()
 
