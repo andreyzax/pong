22c8f02f84e7afd44223aaa07ff66de00ec113bb
Andrey Zax <andrey.zax@gmail.com>
2022-10-18 20:17:42 +0300

    Implement collsions with paddles
    Add score based on successful collsions with the player paddle

--- pong.py
+++ pong.py
@@ -8,7 +8,9 @@ import pygame
 SCREEN_W = 1920
 SCREEN_H = 1200
 
-INITIAL_BALL_SPEED = 10
+INITIAL_BALL_SPEED = 10.0
+MAX_BALL_SPEED     = 20.0
+MAX_BALL_YV        = 15.0
 
 pygame.init()
 
@@ -19,7 +21,12 @@ screen = pygame.display.get_surface()
 bg_color = pygame.Color('grey 12')
 light_grey = pygame.Color(175, 175, 175)
 
+game_font = pygame.font.Font('fonts/bit5x3.ttf', SCREEN_H // 10)
+
 clock = pygame.time.Clock()
+
+
+score = 0
 
 class Background:
 
@@ -53,7 +60,10 @@ class Background:
 
 class Paddle(pygame.sprite.Sprite):
 
-   def __init__(self, x, y, color, step=10): # Coordinates are for the CENTER of the sprite (NOT top right corner).
+   right_nv  = pygame.Vector2(-1,0)
+   left_nv   = pygame.Vector2(1,0)
+
+   def __init__(self, x, y, collision_normal, color, step=10): # Coordinates are for the CENTER of the sprite (NOT top right corner).
 
       super().__init__()
 
@@ -61,6 +71,8 @@ class Paddle(pygame.sprite.Sprite):
       self.image = pygame.surface.Surface((SCREEN_W * 0.005, SCREEN_H * 0.15))
       self.image.fill(color)
       self.rect = self.image.get_rect(center=(x, y))
+      self.nv = collision_normal
+      self.velocity_y = 0
 
 
    def _is_mouse_virtual(self):
@@ -81,10 +93,13 @@ class Paddle(pygame.sprite.Sprite):
       # Move the paddle up & down but limit range of motion to the play_area top and bottom edges
       if pygame.key.get_pressed()[pygame.K_UP]:
          self.rect.top = max((self.rect.top - self.step), background.play_area.top)
+         self.velocity_y = -self.step
       elif pygame.key.get_pressed()[pygame.K_DOWN]:
          self.rect.bottom = min((self.rect.bottom + self.step), background.play_area.bottom)
+         self.velocity_y = self.step
       else: # This being here means that keyboard input gets priority over mouse input
          mouse_y = self._mouse_vert_movment()
+         self.velocity_y = self.step * mouse_y * 0.08
          if mouse_y == 0:
             pass
          elif mouse_y < 0:
@@ -113,6 +128,9 @@ class Ball(pygame.sprite.Sprite):
 
 
    def update(self):
+
+      global score
+
       self.postion += self.velocity
       self.rect.center = (round(self.postion.x), round(self.postion.y))
 
@@ -132,11 +150,25 @@ class Ball(pygame.sprite.Sprite):
             self.velocity.reflect_ip(self.left_nv)
             self.postion += self.velocity
 
+      # Check for collisions with the player
+      if paddle := pygame.sprite.spritecollideany(self, player): # ':=' is a new python 3.8 operator, it does excatly what it looks it's doing
+         score += 1
+         self.velocity.reflect_ip(paddle.nv)
+         self.velocity.y += min(paddle.velocity_y, MAX_BALL_YV, key=abs) # This limit is mostly for playability, the game runs fine without this limit
+                                                                         # but trying to hit the ball when it's moving nearly verticly is very hard.
+
+         self.velocity.scale_to_length(min(MAX_BALL_SPEED, self.velocity.length())) # Make sure to limit the speed (our collision logic doesn't handle high speeds well)
+                                                                                    # We are doing this here since this is the only place we actually *change* the magnitude of the
+                                                                                    # velocity vector (aka the speed). All other collsions just change it's direction
+         #print(self.velocity.length())
+
+         self.postion += self.velocity # Same here as in the border collison checks, prevents overshooting, ball getting "stuck" in a collision state
+
 
 background = Background(bg_color, light_grey)
 
 player = pygame.sprite.GroupSingle()
-player.add(Paddle(background.play_area.left + SCREEN_W * 0.005 * 3, background.play_area.centery, light_grey,
+player.add(Paddle(background.play_area.left + SCREEN_W * 0.005 * 3, background.play_area.centery, Paddle.left_nv ,light_grey,
                   step=background.play_area.height * 0.01))
 
 # Randomize intial ball velocity
@@ -165,6 +197,10 @@ while True:
 
    ball.update()
    ball.draw(screen)
+
+   score_surf = game_font.render(f'{score:02}', False, light_grey)
+   score_rect = score_surf.get_rect(centerx=background.play_area.centerx * 0.9, centery=background.play_area.top + 100)
+   screen.blit(score_surf, score_rect)
 
    pygame.display.update()
 
